5e6fe70ccf63e5c046a6bc25eb85b9efb5e4b0cd
schriftgestalt <georg.seifert@mac.com>
2017-12-19 19:03:56 +0100

    use wordchecker from external file

--- word-o-mat.glyphsPlugin/Contents/Resources/WordOMat.py
+++ word-o-mat.glyphsPlugin/Contents/Resources/WordOMat.py
@@ -26,6 +26,8 @@ try:
 	from robofab.world import CurrentFont
 	from robofab.interface.all.dialogs import Message
 	from random import choice
+	import wordcheck
+	reload(wordcheck)
 	from GlyphsApp import Glyphs
 
 	def setup_binding_CheckBox(self, Object, KeyPath, options = objc.nil):
@@ -59,86 +61,6 @@ except:
 	print '-'*60
 	traceback.print_exc(file=sys.stdout)
 	print '-'*60
-
-class wordChecker(object):
-	def __init__(self, limitToCharset, fontChars, requiredLetters, requiredGroups, bannedLetters, banRepetitions, minLength, maxLength):
-		self.limitToCharset = limitToCharset
-		self.fontChars = fontChars
-		self.requiredLetters = requiredLetters
-		self.requiredGroups = requiredGroups
-		self.bannedLetters = bannedLetters
-		self.banRepetitions = banRepetitions
-		self.minLength = minLength
-		self.maxLength = maxLength
-
-	def excludedAll(self, word, charList):
-		for c in charList:
-			if c in word:
-				return False
-		return True
-
-	def includedAll(self, word, charList):
-		for c in charList:
-			if not c in word:
-				return False
-		return True
-	
-	def includedAny(self, word, charList):
-		if len(charList):
-			for c in charList:
-				if c in word:
-					return True
-			return False
-		else:
-			return True
-	
-	def includedGroups(self, word, charListList):
-		for charList in charListList:
-			if not self.includedAny(word, charList):
-				return False
-		return True
-	
-	def limitedTo(self, word, charList, condition):
-		if condition:
-			for c in word:
-				if not c in charList:
-					return False
-			return True
-		else:
-			return True
-	
-	def uniqueChars(self, word, condition):
-		if condition:
-			wordChars = []
-			for c in word:
-				if c in wordChars:
-					return False
-				wordChars.append(c)
-			return True
-		else:
-			return True
-	
-	def checkLength(self, word):
-		return self.minLength <= len(word) <= self.maxLength
-	
-	def checkExisting(self, word, outputList):
-		return not word in outputList
-	
-	def checkWord(self, word, outputWords):
-		requirements = [
-			(self.checkExisting, [outputWords]),
-			(self.limitedTo, [self.fontChars, self.limitToCharset]),
-			(self.checkLength, []),
-			(self.includedAll, [self.requiredLetters]),
-			(self.includedGroups, [self.requiredGroups]),
-			(self.excludedAll, [self.bannedLetters]),
-			(self.uniqueChars, [self.banRepetitions]),
-		]
-		for reqFunc, args in requirements:
-			if not reqFunc(word, *args):
-				return False
-		return True
-
 
 class WordomatWindow:
 	def __init__(self):
@@ -304,7 +226,7 @@ class WordomatWindow:
 		self.outputWords = [] #initialize/empty
 		Defaults = NSUserDefaults.standardUserDefaults()
 		Defaults.synchronize()
-		checker = wordChecker(self.limitToCharset, self.fontChars, self.requiredLetters, self.requiredGroups, self.bannedLetters, self.banRepetitions, self.minLength, self.maxLength)
+		checker = wordcheck.wordChecker(self.limitToCharset, self.fontChars, self.requiredLetters, self.requiredGroups, self.bannedLetters, self.banRepetitions, self.minLength, self.maxLength)
 		for i in self.allWords:
 			if len(self.outputWords) >= self.wordCount:
 				break
